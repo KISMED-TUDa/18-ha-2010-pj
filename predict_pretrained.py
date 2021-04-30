a7add8915f6310819184ba496aa23ea05c7b67aa
MauriceRohr <34025664+MauriceRohr@users.noreply.github.com>
2021-04-30 18:20:45 +0200

    vorbeugend multiproc auf Win

--- predict_pretrained.py
+++ predict_pretrained.py
@@ -11,9 +11,10 @@ Skript testet das vortrainierte Modell
 from predict import predict_labels
 from wettbewerb import load_references, save_predictions
 
+if __name__ == '__main__':
 
-ecg_leads,ecg_labels,fs,ecg_names = load_references('../test/') # Importiere EKG-Dateien, zugehörige Diagnose, Sampling-Frequenz (Hz) und Name                                                # Sampling-Frequenz 300 Hz
-
-predictions = predict_labels("model_pretrained.npy",ecg_leads,fs,ecg_names)
-
-save_predictions(predictions) # speichert Prädiktion in CSV Datei
+    ecg_leads,ecg_labels,fs,ecg_names = load_references('../test/') # Importiere EKG-Dateien, zugehörige Diagnose, Sampling-Frequenz (Hz) und Name                                                # Sampling-Frequenz 300 Hz
+    
+    predictions = predict_labels("model_pretrained.npy",ecg_leads,fs,ecg_names)
+    
+    save_predictions(predictions) # speichert Prädiktion in CSV Datei
--- train.py
+++ train.py
@@ -13,6 +13,8 @@ from ecgdetectors import Detectors
 import os
 from wettbewerb import load_references
 
+### if __name__ == '__main__':  # bei multiprocessing auf Windows notwendig
+
 ecg_leads,ecg_labels,fs,ecg_names = load_references() # Importiere EKG-Dateien, zugehörige Diagnose, Sampling-Frequenz (Hz) und Name                                                # Sampling-Frequenz 300 Hz
 
 detectors = Detectors(fs)                                 # Initialisierung des QRS-Detektors
