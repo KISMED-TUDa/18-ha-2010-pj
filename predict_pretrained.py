bcb7c58434227aae3e9e74a4b541827e84bd926e
MauriceRohr <34025664+MauriceRohr@users.noreply.github.com>
2022-10-21 14:18:04 +0200

    clean up

--- predict_pretrained.py
+++ predict_pretrained.py
@@ -8,11 +8,6 @@ Skript testet das vortrainierte Modell
 @author: Maurice Rohr
 """
 
-# import socket
-# def guard(*args, **kwargs):
-#     raise Exception("Internet Access Forbidden")
-# socket.socket = guard
-
 from predict import predict_labels
 from wettbewerb import load_references, save_predictions
 import argparse
@@ -24,7 +19,7 @@ if __name__ == '__main__':
     parser.add_argument('--model_name', action='store',type=str,default='model.npy')
     args = parser.parse_args()
     
-    ecg_leads,ecg_labels,fs,ecg_names = load_references(args.test_dir) # Importiere EKG-Dateien, zugehörige Diagnose, Sampling-Frequenz (Hz) und Name                                                # Sampling-Frequenz 300 Hz
+    ecg_leads,ecg_labels,fs,ecg_names = load_references(args.test_dir) # Importiere EKG-Dateien, zugehörige Diagnose, Sampling-Frequenz (Hz) und Name
     
     start_time = time.time()
     predictions = predict_labels(ecg_leads,fs,ecg_names,model_name=args.model_name)
--- train.py
+++ train.py
@@ -15,7 +15,7 @@ from wettbewerb import load_references
 
 ### if __name__ == '__main__':  # bei multiprocessing auf Windows notwendig
 
-ecg_leads,ecg_labels,fs,ecg_names = load_references() # Importiere EKG-Dateien, zugehörige Diagnose, Sampling-Frequenz (Hz) und Name                                                # Sampling-Frequenz 300 Hz
+ecg_leads,ecg_labels,fs,ecg_names = load_references() # Importiere EKG-Dateien, zugehörige Diagnose, Sampling-Frequenz (Hz) und Name (meist fs=300 Hz)
 
 detectors = Detectors(fs)                                 # Initialisierung des QRS-Detektors
 sdnn_normal = np.array([])                                # Initialisierung der Feature-Arrays
