fae38078ad89fb0b5ecdf4c891384cbdb2002aa0
fuerstra <81012277+raoulfuer@users.noreply.github.com>
2022-11-04 19:56:59 +0100

    FFT-Sichtung der Daten

--- Plotter_Daten.m
+++ Plotter_Daten.m
@@ -1,5 +1,5 @@
 %Laden der Daten. Pfad austauschen.%
-load('C:\Users\Raoul\112---Medizin\training\train_ecg_00001.mat');
+load("C:\Users\Raoul\112-Medizin\Training\training\train_ecg_00001.mat");
 %Erzeugen der Zeit Menge t.%
 x = size(val);
 t = (1:x(2));
@@ -9,13 +9,16 @@ plot(t, val(1,:))
 xlabel('Zeit in ?')
 ylabel('Spannung in V')
 title('Daten aus EKG')
-
-
-
+hold on;
+load("C:\Users\Raoul\112-Medizin\Training\training\train_ecg_00010.mat");
+x = size(val);
+t = (1:x(2));
+plot(t, val(1,:))
+legend('1', 'n')
 %Wenn zwei verschiedene EKG's in einem Plot verglichen werden sollen hier
 %zusätzlich Pfad ändern und die Zeilen auskommentieren.%
 
-%hold on;
+
 %load('C:\Users\Raoul\112---Medizin\training\train_ecg_00002.mat');
 %Erzeugen der Zeit Menge t%
 %x = size(val);
